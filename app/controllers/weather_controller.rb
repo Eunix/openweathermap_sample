fea5f4cf7071ab4a556e8b953e4630bd8ffbcb7a
Evgeny Pavlov <evgeny@pavlov.name>
2015-12-07 18:20:22 +0300

    Basic weather info from Web

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,7 @@
 class WeatherController < ApplicationController
   def index
+    if params[:city]
+      @weather = Weather.new(country: params[:country], city: params[:city])
+    end
   end
 end
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,8 +1,15 @@
 class Weather
+  require 'open-uri'
+
   include ActiveModel::Model
 
   attr_accessor :country, :city
 
   def info
+    JSON.load(
+      open(
+        "#{Settings.openweathermap.uri}?APPID=#{Settings.openweathermap.api_token}&q=#{city},#{country}&units=metric"
+      )
+    )
   end
 end
